99deb4cb6353434784f0174102c1066a0e6c5c6f
Iyonmana Nerry <themerrysurprises@gmail.com>
2021-09-20 14:59:55 +0100

    First Commit

---
+++ practice/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDoList, Item
+from .forms import CreateNewList
+
+# Create your views here.
+
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
+
+def create(response):
+    form = CreateNewList()
+    return render(response, "main/create.html", {"forms":form})
